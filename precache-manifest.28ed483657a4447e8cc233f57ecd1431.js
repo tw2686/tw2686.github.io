60be8448acb32625d174e154e3ab229274c2b12f
tw2686 <tw2686@github.com>
2020-01-27 21:08:01 -0500

    Updates

---
+++ precache-manifest.28ed483657a4447e8cc233f57ecd1431.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a34ad6e61c61b8b0ee8231cb0a40fdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6164b29e335cff15b71",
+    "url": "/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "208c6e62919b9ba34009",
+    "url": "/static/css/main.57d2412a.chunk.css"
+  },
+  {
+    "revision": "f6164b29e335cff15b71",
+    "url": "/static/js/2.1becf0e4.chunk.js"
+  },
+  {
+    "revision": "5adaff15f30f8fb7316ca97fba8ef8b8",
+    "url": "/static/js/2.1becf0e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "208c6e62919b9ba34009",
+    "url": "/static/js/main.11569403.chunk.js"
+  },
+  {
+    "revision": "69979021838594e8ccff",
+    "url": "/static/js/runtime-main.440727f7.js"
+  },
+  {
+    "revision": "cfa464d5d713af7f8a34f4f8083ca1ab",
+    "url": "/static/media/JWong_Resume_Spring2020.cfa464d5.pdf"
+  }
+]);
