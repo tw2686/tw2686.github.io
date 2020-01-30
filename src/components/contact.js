4776b84028f56ec5ef90fbdf5a0558adb25340a9
tw2686 <tw2686@github.com>
2020-01-30 05:57:39 -0500

    Fixed home page social links for mobile

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,36 +7,36 @@ class Contact extends Component {
             <div style={{width: '100%', margin: '0', background: '#E8DBD1'}} id="contact">
                 <Grid>
                     <Cell col={12}>
-                <div className="contact-title" >
-                        <h1>Contact</h1>
-                        <hr/>
-                </div>
-                </Cell>
-                <Cell col={12}>
-                <div className="row contact-grid">
-                    <div className="col s12 m12 l4">
-                        {/* Email */}
-                        <a href={"mailto: tw2686@columbia.edu"} rel="noopener noreferrer" target="_blank">
-                            <i className="fa fa-envelope-square" aria-hidden="true"></i>
-                        </a>
-                        <p>tw2686@columbia.edu</p>
-                    </div>
-                    <div className="col s12 m12 l4">
-                        {/* Phone */}
-                        <div>
-                            <i className="fa fa-phone-square" aria-hidden="true"></i>
+                        <div className="contact-title" >
+                            <h1>Contact</h1>
+                            <hr/>
                         </div>
-                        <p>(734) 623-3650</p>
-                    </div>
-                    <div className="col s12 m12 l4">
-                        {/* LinkedIn */}
-                        <a href="https://www.linkedin.com/in/jameswong912/" rel="noopener noreferrer" target="_blank">
-                            <i className="fa fa-linkedin-square" aria-hidden="true"></i>
-                        </a>
-                        <p>LinkedIn://jameswong912</p>
-                    </div>
-                </div>
-                </Cell>
+                    </Cell>
+                    <Cell col={12}>
+                        <div className="row contact-grid">
+                            <div className="col s12 m12 l4">
+                                {/* Email */}
+                                <a href={"mailto: tw2686@columbia.edu"} rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-envelope-square" aria-hidden="true"></i>
+                                </a>
+                                <p>tw2686@columbia.edu</p>
+                            </div>
+                            <div className="col s12 m12 l4">
+                                {/* Phone */}
+                                <div>
+                                    <i className="fa fa-phone-square" aria-hidden="true"></i>
+                                </div>
+                                <p>(734) 623-3650</p>
+                            </div>
+                            <div className="col s12 m12 l4">
+                                {/* LinkedIn */}
+                                <a href="https://www.linkedin.com/in/jameswong912/" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+                                </a>
+                                <p>LinkedIn://jameswong912</p>
+                            </div>
+                        </div>
+                    </Cell>
                 </Grid>
             </div>
         )
--- src/components/home.js
+++ src/components/home.js
@@ -14,26 +14,27 @@ class Home extends Component {
                             <h1>James Wong</h1>
                             <hr/>
                             <p>BS Computer Science, Columbia University, Class of 2020</p>
-                            <Grid className="social-links">
-                                <Cell col={4} phone={12}>
+
+                            <div className=" row social-links">
+                                <div className="col s12 m12 l4">
                                     {/* Email */}
                                     <a href={"mailto: tw2686@columbia.edu"} rel="noopener noreferrer" target="_blank">
                                         <i className="fa fa-envelope-square" aria-hidden="true"></i>
                                     </a>
-                                </Cell>
-                                <Cell col={4} phone={12}>
+                                </div>
+                                <div className="col s12 m12 l4">
                                     {/* LinkedIn */}
                                     <a href="https://www.linkedin.com/in/jameswong912/" rel="noopener noreferrer" target="_blank">
                                         <i className="fa fa-linkedin-square" aria-hidden="true"></i>
                                     </a>
-                                </Cell>
-                                <Cell col={4} phone={12}>
+                                </div>
+                                <div className="col s12 m12 l4">
                                     {/* Github */}
                                     <a href="https://github.com/tw2686" rel="noopener noreferrer" target="_blank">
                                         <i className="fa fa-github-square" aria-hidden="true"></i>
                                     </a>
-                                </Cell>
-                            </Grid>
+                                </div>
+                            </div>
 
                             {/*<div className="home-buttons">
                                 <Button id="resume-button" href={Pdf} rel="noopener noreferrer" target="_blank">Resume</Button>
